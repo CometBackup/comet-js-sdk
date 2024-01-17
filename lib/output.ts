8a0972a3315ebbfe3a14e61a032081b46d6b3da9
Peter Ashford <pashford@cometbackup.com>
2024-01-17 16:29:48 +1300

    comet: sync with comet 23.12.3

--- lib/output.ts
+++ lib/output.ts
@@ -7,7 +7,7 @@
  *
  * @var {string}
  */
-export const APPLICATION_VERSION = "23.12.1";
+export const APPLICATION_VERSION = "23.12.3";
 
 /**
  * APPLICATION_VERSION_MAJOR
@@ -28,7 +28,7 @@ export const APPLICATION_VERSION_MINOR = 12;
  *
  * @var {number}
  */
-export const APPLICATION_VERSION_REVISION = 1;
+export const APPLICATION_VERSION_REVISION = 3;
 
 /**
  * BACKUPJOBAUTORETENTION_AUTOMATIC
