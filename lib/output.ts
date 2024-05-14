57fe40990b2b21b2cceda4adf9be7a8744021689
Ben Frengley <bfrengley@cometbackup.com>
2024-05-14 14:56:28 +1200

    comet: sync with comet 24.3.7

--- lib/output.ts
+++ lib/output.ts
@@ -7,7 +7,7 @@
  *
  * @var {string}
  */
-export const APPLICATION_VERSION = "24.3.6";
+export const APPLICATION_VERSION = "24.3.7";
 
 /**
  * APPLICATION_VERSION_MAJOR
@@ -28,7 +28,7 @@ export const APPLICATION_VERSION_MINOR = 3;
  *
  * @var {number}
  */
-export const APPLICATION_VERSION_REVISION = 6;
+export const APPLICATION_VERSION_REVISION = 7;
 
 /**
  * BACKUPJOBAUTORETENTION_AUTOMATIC
